1f9150a120edc7fb6e01a2f76fa9e8b894ac958a
Nagadivya9515 <yalamarthinagadivya@gmail.com>
2025-10-03 22:34:03 +0530

    Added other amths operators too

--- JSAdd/index.js
+++ JSAdd/index.js
@@ -3,11 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let a = document.getElementById("firstValue");
     let b = document.getElementById("secondValue");
-    let generator = document.getElementById("add");
+    let add_generator = document.getElementById("add_key");
+    let minus_generator = document.getElementById("minus_key");
+    let multiply_generator = document.getElementById("multiply_key");
+    let division_generator = document.getElementById("divide_key");
+    
     let result = document.createElement("p");
-    add_key.addEventListener("click", () => {
+    add_generator.addEventListener("click", () => {
         result.textContent = Number(a.value) + Number(b.value);
-    })
+    });
+    minus_generator.addEventListener("click", () => {
+        result.textContent = Number(a.value) - Number(b.value);
+    });
+    multiply_generator.addEventListener("click", () => {
+        result.textContent = Number(a.value) * Number(b.value);
+    });
+    division_generator.addEventListener("click", () => {
+        result.textContent = Number(a.value) / Number(b.value);
+    });
     document.body.appendChild(result);
 
 })
