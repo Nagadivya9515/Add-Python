b5d12f24564ca770a3e1a2d10c94e5b2c2a2419c
Nagadivya9515 <yalamarthinagadivya@gmail.com>
2025-10-04 21:55:24 +0530

    Fully functional BlackJack game

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,36 +1,40 @@
+
 let a = document.getElementById("firstCard");
 let card1 = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
 let b = document.getElementById("secondCard");
 let card2 = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
 let cardSum = card1 + card2;
+let sumDisplay = document.getElementById("cardSum");
 let btn1 = document.getElementById("getCard1");
 let btn2 = document.getElementById("newCard");
 let message = document.getElementById("message");
 let thirdDiv = document.getElementById("thirdCard");
 let sum = 0;
+let hasBlackJack = false
+let isAlive = true
+
+
 function startGame(){
 {
-    btn1.addEventListener("click", () => {
-        a.textContent = card1;
-        b.textContent = card2;
-        sum = sum + cardSum;
-        if (sum < 21) {
-            message.textContent = "Take a card again and best of luck" + sum;
+        a.textContent = "First Card: " + card1;
+        b.textContent = "Second Card: " + card2;
+        // sum = sum + cardSum;
+        sumDisplay.textContent = "Sum: " +cardSum;
+        if (cardSum < 21) {
+            message.textContent = "Take a card again and best of luck";
         }
-        else if (sum === 21) {
-            message.textContent = "Perfect, You got Jackpot" + sum;
+        else if (cardSum === 21) {
+            message.textContent = "Perfect, You got Jackpot";
             hasBlackJack = true;
         }
         else {
-            message.textContent = "Retry later, You've lost" + sum;
+            message.textContent = "Retry later, You've lost";
             isAlive = false;
         }
     
     if(!isAlive) {
         return
     }
-    });
-    
 }
 }
 
@@ -38,10 +42,27 @@ function newCard()
 {
     let c = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
     let thirdCard = document.createElement('span');
-    sum += c;
-    thirdCard.textContent = c + " " + sum;
-    thirdDiv.appendChild(thirdCard)
+    cardSum += c;
+    sumDisplay.textContent = "Sum: " + cardSum;
+
+    // thirdCard.textContent = c + " " + sum;
+    // thirdDiv.appendChild(thirdCard
     
     startGame();
 
+}
+
+function newGame() {
+    card1 = Math.floor(Math.random() * 10) + 2;
+    card2 = Math.floor(Math.random() * 10) + 2;
+    cardSum = card1 + card2;
+    sum = 0;
+    hasBlackJack = false;
+    isAlive = true;
+
+    a.textContent = ""
+    b.textContent = ""
+    sumDisplay.textContent = ""
+    message.textContent = "New game started, Best of Luck!";
+    thirdDiv.innerHTML = "";
 }
