3e22cb0fcd09e4a54338787846433d56cf9fc4b8
Nagadivya9515 <yalamarthinagadivya@gmail.com>
2025-10-05 17:59:19 +0530

    Calculator operations challenge

--- first-react/my-react-app/src/App.jsx
+++ first-react/my-react-app/src/App.jsx
@@ -30,6 +30,24 @@ function App() {
   return <h1 style={customStyle}>{message}</h1>;
 }
 
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function division(a,b) {
+    return a/b;
+}
+
+export {add, subtract, multiply, division};
   
 
 export default App
--- first-react/my-react-app/src/main.jsx
+++ first-react/my-react-app/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {add, subtract, multiply, division} from "./calculator.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      <p>Addition: {add(1,2)}</p>
+      <p>Subtraction: {subtract(10,2)}</p>
+      <p>Multiply: {multiply(3,2)}</p>
+      <p>Division: {division(10,2)}</p>
     <App />
   </StrictMode>,
 )
